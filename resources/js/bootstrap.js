5c66b1a9f793a80f4ba9038a67318f59d8c35b1b
Deviance <ianrafaelabenido@gmail.com>
2026-02-07 00:29:35 +0800

    Removed unnecessary import

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,8 +8,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allow your team to quickly build robust real-time web applications.
  */
-
-import './echo';
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
