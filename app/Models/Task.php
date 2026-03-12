f34ded265e14615efb58f23d8cccd9a2393c0966
IR-Abenido <ianrafaelabenido@gmail.com>
2026-03-12 11:47:56 +0800

    Updated for task user management

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
@@ -30,13 +31,6 @@ class Task extends Model
     ];
     public $incrementing = false;
 
-    protected static function booted()
-    {
-        static::creating(function ($task) {
-            $task->task_attributes ??= ['backgroundColor' => '#F1F3F5'];
-        });
-    }
-
     public function board(): BelongsTo
     {
         return $this->belongsTo(Board::class, 'board_id');
@@ -54,5 +48,14 @@ class Task extends Model
     public function attachments(): HasMany
     {
         return $this->hasMany(Attachment::class, 'task_id');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'task_members', 'task_id', 'user_id');
+    }
+
+    public function isNotMaxUserLimit(): bool{
+        return $this->members->count() < 3;
     }
 }
